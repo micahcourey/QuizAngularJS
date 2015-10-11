d00bc2c4b194f398cb1bf7438638c7554a491912
micahcourey <micahjcourey@gmail.com>
2015-10-10 22:23:19 -0700

    add to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 (function(){
 	var app = angular.module('quiz', []);
+
+	app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').then(function(quizData){
+				$scope.myQuesions = quizData.data;
+				$scope.totalQuestions = $scope.myQuestions.length;
+		});
+	}]);
 })();
