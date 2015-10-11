46b5ef93d4ff1840e4ba286ce81337a21919a97e
micahcourey <micahjcourey@gmail.com>
2015-10-11 16:53:16 -0700

    add dynamic results info

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@
 				}
 				$scope.myQuestions[qIndex].questionState = 'answered';
 			}
+			$scope.percentage = (($scope.score / $scope.totalQuestions) * 100).toFixed(1);
 		};
 
 		$scope.isSelected = function(qIndex, aIndex){
